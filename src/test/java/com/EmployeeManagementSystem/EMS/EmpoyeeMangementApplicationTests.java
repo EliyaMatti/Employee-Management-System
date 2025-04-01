9ad7e581f7011c9a2d4a4d8d121b83e5923964ed
Eliya <90854031+EliyaMatti@users.noreply.github.com>
2025-04-01 21:50:00 +0530

    inital commit

---
+++ src/test/java/com/EmployeeManagementSystem/EMS/EmpoyeeMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EmployeeManagementSystem.EMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpoyeeMangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
